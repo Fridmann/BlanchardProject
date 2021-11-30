02adc41ed6158e361283b388659d90465da48f3e
Fridmann <besprizzzor@gmail.com>
2021-11-30 19:11:38 +0300

    Commit 10
    проблемы с реализацией пункта 1 и 2

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
-      $('.header__burger,.header__list-top').toggleClass('active');
+      $('.header__burger,.header__list-top,body').toggleClass('active');
     });
     $('.header__list-top li a').click(function(event) {
-      $('.header__burger,.header__list-top').removeClass('active');
+      $('.header__burger,.header__list-top,body').removeClass('active');
     });
   });
--- js/select-galery.js
+++ js/select-galery.js
@@ -2,7 +2,9 @@ const galerySelect = () => {
     const element = document.querySelector('.section-galery__select');
     const choices = new Choices(element, {
         searchEnabled: false,
+        duplicateItemsAllowed: false,
     });
 };
 
 galerySelect();
+
